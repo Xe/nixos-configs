47012f9da6013cddc44138633bf95aac65b227f6
Christine Dodrill <me@christine.website>
2020-03-10 11:30:27 +0000

    nixfmt

--- common/users/cadey/dev.nix
+++ common/users/cadey/dev.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }: {
   services.lorri.enable = true;
-  home.packages = with pkgs; [ cachix direnv niv ];
+  home.packages = with pkgs; [ cachix direnv niv nixfmt ];
 
   programs.fish.interactiveShellInit = ''
     eval (${pkgs.direnv}/bin/direnv hook fish)
--- common/users/cadey/dhall.nix
+++ common/users/cadey/dhall.nix
@@ -3,5 +3,5 @@
 let dhall = import <dhall> { };
 in {
   imports = [ ];
-  home.packages = with dhall; [ linux-dhall linux-dhall-yaml linux-dhall-json ];
+  home.packages = with dhall; [ linux-dhall linux-dhall-yaml linux-dhall-json linux-dhall-nix ];
 }
