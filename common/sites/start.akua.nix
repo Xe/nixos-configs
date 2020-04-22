4c14f6ccfcadfc6e89268fe262c72a1734e87f90
Christine Dodrill <me@christine.website>
2020-04-22 16:48:45 -0400

    sites/start.akua: permalink on github

--- common/sites/start.akua.nix
+++ common/sites/start.akua.nix
@@ -6,7 +6,7 @@ let
     version = "latest";
     src = builtins.fetchurl {
       url =
-        "https://raw.githubusercontent.com/Xe/xepkgs/master/modules/luakit/start.html";
+        "https://raw.githubusercontent.com/Xe/xepkgs/a39de715fdd98639c40b874a4935f0a3e9a7e632/modules/luakit/start.html";
       sha256 = "04bsgcmnj50sh1a61r8bfqrysf34pdc0559hy5vvyljbbcvihkha";
     };
     phases = "installPhase";
