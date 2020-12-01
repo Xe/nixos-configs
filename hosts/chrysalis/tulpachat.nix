be458558d7a80fb328142aac148943e695cd898a
Christine Dodrill <me@christine.website>
2020-12-01 17:27:43 -0500

    chrysalis/tulpachat: use startAt
    Signed-off-by: Christine Dodrill <me@christine.website>

--- hosts/chrysalis/tulpachat.nix
+++ hosts/chrysalis/tulpachat.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  mkUnit = { message, ... }: {
+  mkUnit = { message, sched, ... }: {
     after = [ "tulpachat-webhook-key.service" ];
     wants = [ "tulpachat-webhook-key.service" ];
     serviceConfig.Type = "oneshot";
@@ -16,33 +16,19 @@ let
           -F "username=$USERNAME" \
           "$WEBHOOK"
     '';
+    startAt = sched;
   };
 
-  mkTimer = { sched, name, ... }: {
-    wantedBy = [ "timers.target" ];
-    partOf = [ "tulpachat-${name}-reminder.service" ];
-    timerConfig.OnCalendar = "${sched}";
-  };
 in {
-  systemd = {
-    services = {
-      tulpachat-weekly-reminder = mkUnit {
-        message = "Hey <@&628452770344599573> Time for a weekly progress report!";
-      };
-      tulpachat-monthly-reminder = mkUnit {
-        message = "Hey <@&671224325646450698> Time for a monthly progress report!";
-      };
+  systemd.services = {
+    tulpachat-weekly-reminder = mkUnit {
+      message = "Hey <@&628452770344599573> Time for a weekly progress report!";
+      sched = "Sun *-*-* 16:00:00";
     };
-    timers = {
-      # DayOfWeek Year-Month-Day Hour:Minute:Second
-      tulpachat-weekly-reminder = mkTimer {
-        sched = "Sun *-*-* 16:00:00";
-        name = "weekly";
-      };
-      tulpachat-monthly-reminder = mkTimer {
-        sched = "*-*-1 16:00:00";
-        name = "monthly";
-      };
+    tulpachat-monthly-reminder = mkUnit {
+      message =
+        "Hey <@&671224325646450698> Time for a monthly progress report!";
+      sched = "*-*-1 16:00:00";
     };
   };
 
