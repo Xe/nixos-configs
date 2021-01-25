0938e298e1221be27193eae3b2171211b121004e
Christine Dodrill <me@christine.website>
2021-01-25 14:56:46 -0500

    httpserver
    Signed-off-by: Christine Dodrill <me@christine.website>

--- ops/metadata/peers.nix
+++ ops/metadata/peers.nix
@@ -6,24 +6,27 @@ let
     let
       net = metadata.networks."${network}";
       v6subnet = net.ula;
+      extraAddrs = ({ extra_addrs ? [ ], ... }: extra_addrs) wireguard;
     in {
       allowedIPs = [
         "${metadata.common.ula}:${wireguard.addrs.v6}/128"
         "${metadata.common.gua}:${wireguard.addrs.v6}/128"
         "${wireguard.addrs.v4}/32"
-      ];
+      ] ++ extraAddrs;
       publicKey = wireguard.pubkey;
     };
   serverPeer = { network, wireguard, ip_addr, ... }:
     let
       net = metadata.networks."${network}";
       v6subnet = net.ula;
+      extraAddrs = ({ extra_addrs ? [ ], ... }: extra_addrs) wireguard;
     in {
       allowedIPs = [
         "${metadata.common.ula}:${wireguard.addrs.v6}/128"
         "${metadata.common.gua}:${wireguard.addrs.v6}/128"
         "${wireguard.addrs.v4}/32"
-      ] ++ (if ip_addr == "10.77.3.1" then [ "10.77.0.0/16" ] else [ ]);
+      ] ++ (if ip_addr == "10.77.3.1" then [ "10.77.0.0/16" ] else [ ])
+        ++ extraAddrs;
       publicKey = wireguard.pubkey;
       persistentKeepalive = 25;
       endpoint = "${ip_addr}:${toString wireguard.port}";
@@ -82,6 +85,8 @@ in with metadata.hosts; rec {
     (serverPeer keanu)
     (serverPeer shachi)
     (serverPeer genza)
+    # fake
+    (roamPeer httpserver)
   ];
 
   cloud = [
@@ -98,6 +103,8 @@ in with metadata.hosts; rec {
     (serverPeer lufta)
     (serverPeer firgu)
     (serverPeer kahless)
+    # fake
+    (roamPeer httpserver)
   ];
 
   roadwarrior = [ (serverPeer lufta) (serverPeer firgu) ];
