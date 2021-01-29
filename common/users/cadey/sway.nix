9c3f4f21a6aac17603c63da2ace8df953e4ecbd6
Christine Dodrill <me@christine.website>
2021-01-29 11:20:12 -0500

    colorz
    Signed-off-by: Christine Dodrill <me@christine.website>

--- common/users/cadey/sway.nix
+++ common/users/cadey/sway.nix
@@ -7,91 +7,113 @@ in {
       enable = true;
       wrapperFeatures.gtk = true;
       # emulate my dwm config
-      config = with pkgs; {
-        terminal = "${pkgs.nur.repos.xe.st}";
-        bars = [{
-          fonts = [ "Dunda 8" "Hack 8" ];
-          colors = {
-            background = "#282828";
-            statusline = "#ebdbb2";
-            separator = "#666666";
-            focusedWorkspace = {
-              border = "#d3869b";
+      config = with pkgs;
+        let
+          focused = rec {
+            border = "#d3869b";
+            background = border;
+            text = "#fbf1c7";
+          };
+          active = rec {
+            border = "#b16286";
+            background = border;
+            text = "#fbf1c7";
+          };
+        in {
+          terminal = "${pkgs.nur.repos.xe.st}";
+          bars = [{
+            fonts = [ "Dunda 8" "Hack 8" ];
+            colors = {
               background = "#282828";
-              text = "#fbf1c7";
+              statusline = "#ebdbb2";
+              separator = "#666666";
+              focusedWorkspace = focused;
+              activeWorkspace = active;
+              inactiveWorkspace = {
+                border = "#333333";
+                background = "#222222";
+                text = "#888888";
+              };
+              urgentWorkspace = {
+                border = "#2f343a";
+                background = "#900000";
+                text = "#ffffff";
+              };
+              bindingMode = {
+                border = "#2f343a";
+                background = "#900000";
+                text = "#ffffff";
+              };
             };
-            activeWorkspace = {
-              border = "#b16286";
-              background = "#1d2021";
+            command = "${pkgs.sway}/bin/swaybar";
+            hiddenState = "hide";
+            mode = "dock";
+            position = "top";
+            statusCommand = "${pkgs.i3status}/bin/i3status";
+            workspaceButtons = true;
+            trayOutput = "primary";
+          }];
+          colors = {
+            focused = rec {
+              background = "#d3869b";
+              border = background;
+              text = "#32302f";
+              indicator = "#2e9ef4";
+              childBorder = "#3d3285";
+            };
+            focusedInactive = rec {
+              background = "#b16286";
+              border = background;
               text = "#ebdbb2";
+              indicator = "#b57614";
+              childBorder = "#3d3285";
             };
-            inactiveWorkspace = {
-              border = "#333333";
-              background = "#222222";
-              text = "#888888";
-            };
-            urgentWorkspace = {
-              border = "#2f343a";
-              background = "#900000";
-              text = "#ffffff";
-            };
-            bindingMode = {
-              border = "#2f343a";
-              background = "#900000";
-              text = "#ffffff";
+            unfocused = rec {
+              background = "#8f3f71";
+              border = background;
+              text = "#ebdbb2";
+              indicator = "#b57614";
+              childBorder = "#3d3285";
             };
           };
-          command = "${pkgs.sway}/bin/swaybar";
-          hiddenState = "hide";
-          mode = "dock";
-          position = "top";
-          statusCommand = "${pkgs.i3status}/bin/i3status";
-          workspaceButtons = true;
-          trayOutput = "primary";
-        }];
-        floating = {
-          criteria =
-            [ { title = "Steam - Update News"; } { class = "Pavucontrol"; } ];
-          titlebar = true;
-        };
-        focus = { followMouse = "yes"; };
-        fonts = [ "Dunda 8" "Hack 8" ];
-        gaps = {
-          horizontal = 6;
-          vertical = 6;
-          inner = 12;
-          smartBorders = "on";
-          smartGaps = true;
-        };
-        keybindings =
-          let modifier = config.wayland.windowManager.sway.config.modifier;
-          in lib.mkOptionDefault {
-            "${modifier}+Shift+Return" = "exec ${pkgs.nur.repos.xe.st}/bin/st";
-            "${modifier}+Shift+c" = "kill";
-            "${modifier}+p" =
-              "exec ${pkgs.dmenu}/bin/dmenu_path | ${pkgs.dmenu}/bin/dmenu -nb '#1d2021' -nf '#ebdbb2' -sb '#b16286' -sf '#fbf1c7' | ${pkgs.findutils}/bin/xargs swaymsg exec --";
-            "${modifier}+t" = "layout toggle split";
-            "${modifier}+f" = "layout tabbed";
-            "${modifier}+u" = "layout stacking";
-            "${modifier}+e" = "exec $home/bin/e";
+          floating = {
+            criteria =
+              [ { title = "Steam - Update News"; } { class = "Pavucontrol"; } ];
+            titlebar = true;
           };
-        output = nixosConfig.cadey.sway.output;
-        startup = [
-          {
-            command = "systemctl --user restart waybar";
-            always = true;
-          }
-          {
-            command = "firefox";
-            always = true;
-          }
-          {
-            command = "Discord";
-            always = true;
-          }
-        ];
-        window = { border = 1; };
-      };
+          focus = { followMouse = "yes"; };
+          fonts = [ "Dunda 8" "Hack 8" ];
+          gaps = {
+            horizontal = 6;
+            vertical = 6;
+            inner = 12;
+            smartBorders = "on";
+            smartGaps = true;
+          };
+          keybindings =
+            let modifier = config.wayland.windowManager.sway.config.modifier;
+            in lib.mkOptionDefault {
+              "${modifier}+Shift+Return" =
+                "exec ${pkgs.nur.repos.xe.st}/bin/st";
+              "${modifier}+Shift+c" = "kill";
+              "${modifier}+p" =
+                "exec ${pkgs.dmenu}/bin/dmenu_path | ${pkgs.dmenu}/bin/dmenu -nb '#1d2021' -nf '#ebdbb2' -sb '#b16286' -sf '#fbf1c7' | ${pkgs.findutils}/bin/xargs swaymsg exec --";
+              "${modifier}+t" = "layout toggle split";
+              "${modifier}+f" = "layout tabbed";
+              "${modifier}+u" = "layout stacking";
+              "${modifier}+e" = "exec $home/bin/e";
+            };
+          output = nixosConfig.cadey.sway.output;
+          startup = [
+            {
+              command = "systemctl --user restart waybar";
+              always = true;
+            }
+            { command = "firefox"; }
+            { command = "Discord"; }
+          ];
+          window = { border = 1; };
+        };
       extraSessionCommands = ''
         export SDL_VIDEODRIVER=wayland
         # needs qt5.qtwayland in systemPackages
