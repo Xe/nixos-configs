1b2715f404ba972d52ecc9a17dd0565ae6165764
Christine Dodrill <me@christine.website>
2021-02-06 11:35:19 -0500

    update xesite
    Signed-off-by: Christine Dodrill <me@christine.website>

--- ops/metadata/peers.nix
+++ ops/metadata/peers.nix
@@ -46,33 +46,6 @@ let
       listenPort = wireguard.port;
       inherit peers;
     };
-  mkClientConfig = { network, wireguard, ... }:
-    name:
-    let
-      net = metadata.networks."${network}";
-      v6subnet = net.ula;
-    in with metadata.hosts;
-    writeTextFile {
-      name = "${name}.conf";
-      text = ''
-        [Interface]
-        Address = ${wireguard.addrs.v4}/16, ${metadata.common.ula}:${wireguard.addrs.v6}/48, "${metadata.common.gua}:${wireguard.addrs.v6}/48
-        ListenPort = 0
-        DNS = ${firgu.wireguard.addrs.v4}
-        PrivateKey = FILL_ME_IN
-
-        # firgu is the primary router
-        [Peer]
-        PublicKey = ${firgu.wireguard.pubkey}
-        AllowedIPs = ${metadata.common.v4}/16, ${metadata.common.ula}::/48, ${metadata.common.gua}::/48
-        Endpoint = ${firgu.ip_addr}:${toString firgu.wireguard.port}
-
-        [Peer]
-        PublicKey = ${lufta.wireguard.pubkey}
-        AllowedIPs = ${lufta.wireguard.addrs.v4}/32, ${metadata.common.ula}:${lufta.wireguard.addrs.v6}/128, ${metadata.common.gua}:${lufta.wireguard.addrs.v6}/128
-        Endpoint = ${lufta.ip_addr}:${toString lufta.wireguard.port}
-      '';
-    };
 in with metadata.hosts; rec {
   # expected peer lists
   hexagone = [
@@ -90,10 +63,6 @@ in with metadata.hosts; rec {
   ];
 
   cloud = [
-    # roadwarrior
-    (roamPeer la-tahorskami)
-    (roamPeer la-selbeifonxa)
-    (roamPeer tolsutra)
     # hexagone
     (roamPeer chrysalis)
     (roamPeer keanu)
@@ -121,11 +90,5 @@ in with metadata.hosts; rec {
 
     # roadwarrior
     genza = interfaceInfo genza hexagone;
-  };
-
-  confs = {
-    la-tahorskami = mkClientConfig la-tahorskami "la-tahorskami";
-    la-selbeifonxa = mkClientConfig la-selbeifonxa "la-selbeifonxa";
-    tolsutra = mkClientConfig tolsutra "tolsutra";
   };
 }
