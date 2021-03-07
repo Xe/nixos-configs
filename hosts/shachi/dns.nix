9890982e662f8b4e1775b7e286006417ccf81bcd
Christine Dodrill <me@christine.website>
2021-03-07 13:41:13 -0500

    fix dns
    Signed-off-by: Christine Dodrill <me@christine.website>

--- hosts/shachi/dns.nix
+++ hosts/shachi/dns.nix
@@ -1,3 +1,3 @@
 { lib, ... }: {
-  networking.nameservers = lib.mkForce [ "100.100.100.100" "10.77.2.2" "8.8.8.8" "1.1.1.1" ];
+  networking.nameservers = lib.mkForce [ "100.100.100.100" "8.8.8.8" "1.1.1.1" ];
 }
