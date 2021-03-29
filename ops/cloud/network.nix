b8bd0c864ce477805fc26f67e416b1b15d6d060f
Christine Dodrill <me@christine.website>
2021-03-29 07:35:57 -0400

    more curses being lifted every second
    Signed-off-by: Christine Dodrill <me@christine.website>

--- ops/cloud/network.nix
+++ ops/cloud/network.nix
@@ -1,14 +1,32 @@
 {
   network = {
     description = "Within's Cloud Hosting Fortress of Invincibility";
-    deployment.targetUser = "root";
   };
 
   "kahless" = { config, pkgs, lib, ... }:
     let metadata = pkgs.callPackage ../metadata/peers.nix { };
     in {
+      deployment.targetUser = "root";
       deployment.targetHost = metadata.raw.kahless.ip_addr;
 
       imports = [ ../../hosts/kahless/configuration.nix ];
+    };
+
+  "firgu" = { config, pkgs, lib, ... }:
+    let metadata = pkgs.callPackage ../metadata/peers.nix { };
+    in {
+      deployment.targetUser = "root";
+      deployment.targetHost = metadata.raw.firgu.ip_addr;
+
+      imports = [ ../../hosts/firgu/configuration.nix ];
+    };
+
+  "lufta" = { config, pkgs, lib, ... }:
+    let metadata = pkgs.callPackage ../metadata/peers.nix { };
+    in {
+      deployment.targetUser = "root";
+      deployment.targetHost = metadata.raw.lufta.ip_addr;
+
+      imports = [ ../../hosts/lufta/configuration.nix ];
     };
 }
