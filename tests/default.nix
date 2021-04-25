74024675d14c4d1d4174cdb2be6ed811ae30f325
Christine Dodrill <me@christine.website>
2021-04-25 16:20:03 -0400

    run tests in CI
    Signed-off-by: Christine Dodrill <me@christine.website>

---
+++ tests/default.nix
@@ -0,0 +1,5 @@
+{
+  pahi = import ./pahi.nix;
+  todayinmarch2020 = import ./todayinmarch2020.nix;
+  within-homedir = import ./within-homedir.nix;
+}
--- tests/todayinmarch2020.nix
+++ tests/todayinmarch2020.nix
@@ -3,7 +3,7 @@ in import "${pkgs}/nixos/tests/make-test-python.nix" ({ pkgs, ... }: {
   system = "x86_64-linux";
 
   nodes.machine = { config, pkgs, ... }: {
-    imports = [ ../common/base.nix ];
+    imports = [ ../common ];
     virtualisation.graphics = false;
 
     within.services.todayinmarch2020.enable = true;
